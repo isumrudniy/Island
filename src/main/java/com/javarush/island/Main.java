c8939907f19ada2855a0f51fd5133c41e34eb0ce
darkisu <isumrudniy@me.com>
2023-09-02 12:33:49 +0500

    * version 0.4

--- src/main/java/com/javarush/island/Main.java
+++ src/main/java/com/javarush/island/Main.java
@@ -1,5 +1,7 @@
 package com.javarush.island;
 
+import com.javarush.island.entities.animals.predators.Wolf;
+
 public class Main {
     public static void main(String[] args) {
         Location[][] gameMap = new Location[5][5];
@@ -12,12 +14,18 @@ public class Main {
             }
         }
 
+        // Для проверки реализации
+
         for (var map: gameMap
              ) {
             for (var loc: map
                  ) {
                 System.out.println(loc);
+                System.out.println(loc.getX() + " " + loc.getY());
+                loc.getEntitiesList().forEach(entity -> System.out.println(entity instanceof Wolf));
+                System.out.println("===========================");
             }
+            System.out.println();
             System.out.println();
         }
 
--- src/main/java/com/javarush/island/entities/Entities.java
+++ src/main/java/com/javarush/island/entities/Entities.java
@@ -1,4 +1,22 @@
 package com.javarush.island.entities;
 
-public class Entities {
+import com.javarush.island.behavior.Eatable;
+import com.javarush.island.behavior.Movable;
+import com.javarush.island.behavior.Reproducible;
+
+public abstract class Entities implements Eatable, Movable, Reproducible {
+    @Override
+    public void eat() {
+
+    }
+
+    @Override
+    public void move() {
+
+    }
+
+    @Override
+    public void reproduce() {
+
+    }
 }
