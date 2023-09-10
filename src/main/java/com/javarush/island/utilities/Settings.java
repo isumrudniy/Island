f1eba9c4862a1dd36f6ea190c33fd75079e2e246
darkisu <isumrudniy@me.com>
2023-09-10 21:17:24 +0500

    * version 1.0

--- src/main/java/com/javarush/island/utilities/Settings.java
+++ src/main/java/com/javarush/island/utilities/Settings.java
@@ -12,7 +12,7 @@ import java.util.stream.Stream;
 
 public class Settings {
 
-    public static final String SALUTATION = "Приветствую тебя, мой дорогой друг!" +
+    public static final String SALUTATION = "Приветствую тебя, мой дорогой друг! " +
             "Приступаем к созданию острова!";
     public static final String CREATE_MAP = "Введите размер карты.";
     public static final String INPUT_ROWS = "Введите длину острова: ";
